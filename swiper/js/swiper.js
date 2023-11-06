d9447cc30b01a90b1621f3313b8258d8bc3889ea
jiyy25 <jiyy825@gmail.com>
2023-11-06 12:28:17 +0900

    swiper fade

--- swiper/js/swiper.js
+++ swiper/js/swiper.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    // setInterval, animate, css , appendTo
+    // // setInterval, animate, css , appendTo
 
     let count = 0;
 
@@ -8,10 +8,19 @@ $(document).ready(function () {
 
     function fadeani() { //선언적함수
         count++;
-        count = count % 3;
+        count = count % $(".swiper-slide").length;
+        //계속 반복..!
         $(".swiper-slide").eq(count).addClass("on").siblings().removeClass("on");
     }
+    // const anih = $(".swiper-slide").height();
 
+    // setInterval(function () {
+    //     $(".swiper-wrapper").animate({ "marginTop": -anih }, 300, function () {
+    //         $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
+    //         $(".swiper-wrapper").css("marginTop", 0)
+    //     })
+    // }, 3000);
+   
 })
 
 
