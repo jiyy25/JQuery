6dc9fbb08bcff0a22d6f5706c069715a22d62e08
jiyy25 <jiyy825@gmail.com>
2023-11-06 11:11:39 +0900

    swiper 세로 수정해야함

--- swiper/js/swiper.js
+++ swiper/js/swiper.js
@@ -2,16 +2,15 @@ $(document).ready(function () {
 
     // setInterval, animate, css , appedTo
 
-    const aniw = $(".swiper-slide").width();
-    //$("swiper-slide").css("width");
-    //워낙 많이 써서 저런 표현식이 가능하게 됨... 값이 없으면 그대로 가져옴
+    const anih = $(".swiper-slide").height();
 
     setInterval(function () {
-        $(".swiper-wrapper").animate({ "marginLeft": -aniw }, 400, function () {
+        $(".swiper-wrapper").amimate({ "marginTop": -anih }, 400, function () {
             $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
-            $(".swiper-wrapper").css("marginLeft", 0)
+            $(".swiper-wrapper").css("marginTop", 0)
         })
-    }, 3000)
+    })
+
 })
 
 
